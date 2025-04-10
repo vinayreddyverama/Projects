73a2c7bb660786cbdf60c54df29d6069eb92e318
vinayreddyverama <vinayreddyveramavinayreddyveramareddy@gmail.com>
2025-04-09 17:11:32 -0700

    Array
    codesignal question

---
+++ Pratice/arrayp1.py
@@ -0,0 +1,20 @@
+s=[2,83,1,11,13,14]
+s=[2,50,83,30]
+# count the number of pairs in the list
+def counting(s):
+    l=len(s)
+    count=0
+    t=[0]*l
+    for i in range(l):
+        if s[i]%10==0:
+            t[i]=s[i]//10
+        else:
+            t[i]=s[i]
+    print(t)
+    for i in range(l):
+        for j in range(len(s)):
+            if s[i]+s[j]==t[j]+t[i]:
+                print(s[i],s[j],t[j],t[i])
+                count+=1
+    return count
+print("Output Count:",counting(s))
